3aa4fe9d7378c6237b93a3be3ef778d347878c17
AedenMoylan <aedenmoylan@gmail.com>
2021-10-11 21:35:04 +0100

    added finished Q2

--- data structures lab 1/data structures lab 1/Source.cpp
+++ data structures lab 1/data structures lab 1/Source.cpp
@@ -17,17 +17,38 @@ T scalerProduct(std::vector<T> vec1, std::vector<T> vec2)
 
 
 template<typename T1>
-T1 minMax(std::vector<T1> vec)
+//T1 minMax(std::vector<T1> vec)
+//{
+//	int size = vec.size();
+//	int lowest = 0;
+//	int highest = 0;
+//
+//	for (int i = 0; i < size; i++)
+//	{
+//
+//	}
+//
+//}
+
+std::pair<T1, T1> minMax(std::vector<T1> const& t_vec)
 {
-	int size = vec.size();
-	int lowest = 0;
-	int highest = 0;
+	int size = t_vec.size();
+	T1 lowest = 99999;
+	T1 highest = 0;
 
 	for (int i = 0; i < size; i++)
 	{
-
+		if (t_vec[i] < lowest)
+		{
+			lowest = t_vec[i];
+		}
+		if (t_vec[i] > highest)
+		{
+			highest = t_vec[i];
+		}
 	}
 
+	return std::pair<T1, T1> { lowest, highest };
 }
 
 int main()
@@ -38,6 +59,10 @@ int main()
 	std::vector<int> bigVec = { 5,2,7,1,9,8 };
 
 	int result = scalerProduct(v1, v2);
-	int result2 = minMax(bigVec);
-	std::cout << result2 << std::endl;
+
+	std::pair<double, double> result2 =  minMax(bigVec);
+
+	std::cout << result2.first << std::endl;
+	std::cout << result2.second << std::endl;
+	/*std::cout << result2 << std::endl;*/
 }
